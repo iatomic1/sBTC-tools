11402c0212ae68c8b9ec9466f1620b81672cf4fc
adeyemialameen04 <atomic.k.2739@gmail.com>
2025-03-30 20:58:48 +0100

    feat: implemented web landing page

---
+++ apps/web/src/config/site.ts
@@ -0,0 +1,9 @@
+const siteConfig = {
+  title: 'sBTC-Tools',
+  analytics: 'https://sbtc-analytics.vercel.app/',
+  socials: {
+    github: 'https://github.com/iatomic1/sBTC-tools/',
+    twitter: 'https://x.com/iatomic_1',
+  },
+};
+export default siteConfig;
